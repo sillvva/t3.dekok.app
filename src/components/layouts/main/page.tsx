acd160a821626526d99322eb01fb7aa202de683e
Matt Dekok <matt.dekok@gmail.com>
2022-07-21 12:58:03 -0500

    About Me

--- src/components/layouts/main/page.tsx
+++ src/components/layouts/main/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import type { FunctionComponent, PropsWithChildren } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { mainMotion } from "$src/layouts/main";
-// import styles from "../../../layouts/main/MainLayout.module.scss";
+import styles from "$src/layouts/main/styles.module.scss";
 import { conClasses } from "$src/utils/misc";
 
 type PageBgProps = {
@@ -31,57 +31,68 @@ const PageBg: FunctionComponent<PageBgProps> = ({ theme, init }) => {
   );
 };
 
-// const PageBody = (props: PropsWithChildren<unknown>) => {
-//   return <div className={styles.PageBody}>{props.children}</div>;
-// };
+const PageBody = (props: PropsWithChildren<unknown>) => {
+  return <div className={styles.PageBody}>{props.children}</div>;
+};
 
-// interface PageArticleProps {
-//   className?: string;
-// }
+interface PageArticleProps {
+  className?: string;
+}
 
-// const PageArticle = (props: PropsWithChildren<PageArticleProps>) => {
-//   return <article className={conClasses([styles.PageArticle, props.className || ""])}>{props.children}</article>;
-// };
+const PageArticle = (props: PropsWithChildren<PageArticleProps>) => {
+  return (
+    <article className={conClasses(["shadow-xl ring-1 rounded-lg mb-4 ring-gray-900/5 overflow-hidden bg-theme-article", props.className || ""])}>
+      {props.children}
+    </article>
+  );
+};
 
-// interface PageSectionProps {
-//   className?: string;
-//   bgImage?: string;
-// }
+interface PageSectionProps {
+  className?: string;
+  bgImage?: string;
+}
 
-// const PageSection = (props: PropsWithChildren<PageSectionProps>) => {
-//   return (
-//     <section className={conClasses([props.className || "", props.bgImage ?? `bg-[image:url(${props.bgImage}))]`, `bg-cover bg-center`])}>
-//       {props.children}
-//     </section>
-//   );
-// };
+const PageSection = (props: PropsWithChildren<PageSectionProps>) => {
+  return (
+    <section
+      className={conClasses([
+        "p-5 section-divider text-theme-base",
+        "last:border-b-0",
+        props.className || "",
+        props.bgImage ?? `bg-[image:url(${props.bgImage}))]`,
+        `bg-cover bg-center`
+      ])}>
+      {props.children}
+    </section>
+  );
+};
 
-// const PageSectionItems = (props: PropsWithChildren<unknown>) => {
-//   return <div className={styles.SectionItems}>{props.children}</div>;
-// };
+const PageSectionItems = (props: PropsWithChildren<unknown>) => {
+  return <div className={styles.SectionItems}>{props.children}</div>;
+};
 
-// type PageSectionItemProps = {
-//   image: string;
-// };
+type PageSectionItemProps = {
+  image: string;
+};
 
-// const PageSectionItem = (props: PropsWithChildren<PageSectionItemProps>) => {
-//   return (
-//     <div className={styles.SectionItem}>
-//       <div className={styles.SectionItem__Image}>
-//         <Image src={props.image} alt="" layout="fill" objectFit="contain" />
-//       </div>
-//       <div className={styles.SectionItem__Content}>{props.children}</div>
-//     </div>
-//   );
-// };
+const PageSectionItem = (props: PropsWithChildren<PageSectionItemProps>) => {
+  return (
+    <div className={styles.SectionItem}>
+      <div className={styles.SectionItem__Image}>
+        <Image src={props.image} alt="" layout="fill" objectFit="contain" />
+      </div>
+      <div className={styles.SectionItem__Content}>{props.children}</div>
+    </div>
+  );
+};
 
 const PageComponents = {
   Bg: PageBg,
-  // Body: PageBody,
-  // Article: PageArticle,
-  // Section: PageSection,
-  // SectionItems: PageSectionItems,
-  // SectionItem: PageSectionItem
+  Body: PageBody,
+  Article: PageArticle,
+  Section: PageSection,
+  SectionItems: PageSectionItems,
+  SectionItem: PageSectionItem
 };
 
 export default PageComponents;
