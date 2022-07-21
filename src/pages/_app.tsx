3d21c4336e3979bf1ca6b6352e20fd63502fdfcf
Matt Dekok <matt.dekok@gmail.com>
2022-07-20 23:06:34 -0500

    Tweaks

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,10 +10,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
 };
 
 const getBaseUrl = () => {
-  if (typeof window !== "undefined") {
-    return "";
-  }
-  if (process.browser) return ""; // Browser should use current path
+  if (typeof window !== "undefined") return "";
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
 
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
