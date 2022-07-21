a13e68c0c41e1f27c460542e0e806e3501063606
Matt Dekok <matt.dekok@gmail.com>
2022-07-20 22:04:29 -0500

    Initial Commit

---
+++ src/server/router/posts.ts
@@ -0,0 +1,9 @@
+import { createRouter } from "./context";
+import { z } from "zod";
+
+export const postsRouter = createRouter()
+  .query("get", {
+    async resolve({ ctx }) {
+      return await ctx.prisma.blog.findMany();
+    },
+  });
