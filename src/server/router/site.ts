17d09943aae2aed856c19d4df6e11b699065b0fe
Matt Dekok <matt.dekok@gmail.com>
2022-07-22 13:57:57 -0500

    siteRouter mutation

--- src/server/router/site.ts
+++ src/server/router/site.ts
@@ -2,41 +2,40 @@ import { createRouter } from "./context";
 import { z } from "zod";
 import { env } from "../env.mjs";
 
-export const siteRouter = createRouter()
-  .query("revalidate", {
-    input: z.object({
-      paths: z.array(z.string())
-    }),
-    async resolve({ input, ctx }) {
-      const { req, res } = ctx;
-      if (!req || !res) return { revalidated: [] };
+export const siteRouter = createRouter().mutation("revalidate", {
+  input: z.object({
+    paths: z.array(z.string())
+  }),
+  async resolve({ input, ctx }) {
+    const { req, res } = ctx;
+    if (!req || !res) return { revalidated: [] };
 
-      const revalidated = [];
-      const errors = [];
-      if (env.PROD_URL.includes(req.headers.host || "xN/Ax")) {
-        for (let path of input.paths) {
-          try {
-            await res.revalidate(path);
-            revalidated.push(path);
-          }
-          catch(err: any) {
-            errors.push(err.message);
-          }
-        }
-      } else {
+    const revalidated = [];
+    const errors = [];
+    if (env.PROD_URL.includes(req.headers.host || "xN/Ax")) {
+      for (let path of input.paths) {
         try {
-          const data = {"0":{json:{paths:input.paths}}};
-          await fetch(`${env.PROD_URL}/api/trpc/site.revalidate?batch=1&input=${encodeURIComponent(JSON.stringify(data))}`);
-          revalidated.push(...input.paths);
-        }
-        catch(err: any) {
+          await res.revalidate(path);
+          revalidated.push(path);
+        } catch (err: any) {
           errors.push(err.message);
         }
       }
-
-      return {
-        revalidated,
-        errors
-      };
+    } else {
+      try {
+        await fetch(`${env.PROD_URL}/api/trpc/site.revalidate?batch=1`, {
+          method: "POST",
+          body: JSON.stringify({ "0": { json: { paths: input.paths } } })
+        });
+        revalidated.push(...input.paths);
+      } catch (err: any) {
+        errors.push(err.message);
+      }
     }
-  });
+
+    return {
+      revalidated,
+      errors
+    };
+  }
+});
