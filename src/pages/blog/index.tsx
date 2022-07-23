38b179d3919f874f1b9aca3181af2ed45533104c
Matt Dekok <matt.dekok@gmail.com>
2022-07-22 19:13:56 -0500

    Post Loaders

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -19,7 +19,7 @@ const Blog: NextPageWithLayout = () => {
   const limit = parseInt(query.limit ? (Array.isArray(query.limit) ? query.limit[0] : query.limit) : itemsPerPage.toString());
   const { data: posts } = trpc.useQuery(["posts.get"]);
 
-  if (!posts) return <PageMessage>Loading...</PageMessage>;
+  if (!posts) return <div className="grid gap-3 pt-3 mt-3 mx-auto md:grid-cols-2 xl:grid-cols-3 max-w-8xl">{Array(itemsPerPage).fill(<PostLoader />)}</div>;
   if (!posts.length) return <PageMessage>No posts found</PageMessage>;
 
   const paginatedPosts = posts.slice((page - 1) * limit, page * limit);
@@ -89,5 +89,31 @@ export const PostCard = (post: inferQueryOutput<"posts.get">[number]) => {
         {ripples}
       </a>
     </Link>
+  );
+};
+
+export const PostLoader = () => {
+  return (
+    <div
+      className={concatenate(
+        "flex justify-start duration-500 bg-theme-article rounded-lg overflow-hidden",
+        "shadow-md shadow-black/15 motion-safe:hover:scale-105 hover:shadow-lg hover:shadow-black/25"
+      )}>
+      <div className="relative h-full min-w-[8rem] max-w-[8rem] motion-safe:animate-pulse bg-gray-500/50"></div>
+      <div className="flex flex-col flex-1 p-4 gap-2">
+        <div className="w-2/3 h-6 flex items-center">
+          <span className="motion-safe:animate-pulse bg-gray-500/50 block overflow-hidden w-full h-full rounded-full bg-theme-hover bg-opacity-15"></span>
+        </div>
+        <div className="w-1/2 h-4 flex items-center">
+          <span className="motion-safe:animate-pulse bg-gray-500/50 block overflow-hidden w-full h-full rounded-full bg-theme-hover bg-opacity-15"></span>
+        </div>
+        <div className="w-full h-4 flex items-center">
+          <span className="motion-safe:animate-pulse bg-gray-500/50 block overflow-hidden w-full h-full rounded-full bg-theme-hover bg-opacity-15"></span>
+        </div>
+        <div className="w-full h-4 flex items-center">
+          <span className="motion-safe:animate-pulse bg-gray-500/50 block overflow-hidden w-full h-full rounded-full bg-theme-hover bg-opacity-15"></span>
+        </div>
+      </div>
+    </div>
   );
 };
