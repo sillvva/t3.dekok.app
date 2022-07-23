19a61faa8aa37c71e1ef4b3df887e8cb305e9adf
Matt Dekok <matt.dekok@gmail.com>
2022-07-22 21:03:57 -0500

    Remove gallery styles

--- src/components/gallery/index.tsx
+++ src/components/gallery/index.tsx
@@ -1,8 +1,5 @@
-import Image from "next/image";
+import Image from "next/future/image";
 import { PropsWithChildren } from "react";
-import styles from "./Gallery.module.scss";
-
-export const galleryStyles = styles;
 
 type GalleryItemProps = {
   link: string | null | undefined;
@@ -16,12 +13,18 @@ function GalleryItem(props: GalleryItemProps) {
   return (
     <GalleryItemWrapper link={props.link || ""}>
       {props.image && (
-        <Image src={props.image.startsWith("http") ? props.image : `/images/preview-${props.image}.jpg`} alt={props.title} width={400} height={400} />
+        <Image
+          src={props.image.startsWith("http") ? props.image : `/images/preview-${props.image}.jpg`}
+          alt={props.title}
+          className="object-cover object-center max-h-full"
+          width={500}
+          height={500}
+        />
       )}
-      <div className={styles.Cover}>
-        <h3>{props.title}</h3>
-        {props.subtitle && <h4>{props.subtitle}</h4>}
-        {props.description && <div>{props.description}</div>}
+      <div className="absolute inset-0 m-2 p-2 flex flex-col justify-end bg-gradient-to-b from-transparent to-black text-white">
+        <h3 className="text-lg font-medium my-0 drop-shadow-sm">{props.title}</h3>
+        {props.subtitle && <h4 className="text-base font-light m-0 drop-shadow-sm">{props.subtitle}</h4>}
+        {props.description && <div className="text-sm font-light mt-1 drop-shadow-sm">{props.description}</div>}
       </div>
     </GalleryItemWrapper>
   );
@@ -36,11 +39,11 @@ export type GalleryItemWrapperProps = {
 function GalleryItemWrapper(props: PropsWithChildren<GalleryItemWrapperProps>) {
   if (props.link) {
     return (
-      <a href={props.link} target="_blank" rel="noopener noreferrer" className={styles.LinkedGalleryItem}>
+      <a href={props.link} target="_blank" rel="noopener noreferrer" className="gallery-item hover:shadow-xl motion-safe:hover:scale-105">
         {props.children}
       </a>
     );
   }
 
-  return <div className={styles.GalleryItem}>{props.children}</div>;
+  return <div className="gallery-item">{props.children}</div>;
 }
